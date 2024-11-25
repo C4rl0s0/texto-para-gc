9ae2b8bb32918846ea1cf260c077d05348442694
C4rl0s0 <64045901+C4rl0s0@users.noreply.github.com>
2024-11-25 19:25:03 -0300

    Update json_to_rdf.py

--- json_to_rdf.py
+++ json_to_rdf.py
@@ -29,24 +29,23 @@ rdf_file = open("graph.txt", "w")
 x = f.read()
 relations = json.loads(x)
 entities = dict()
+disambiguities = dict()
 uri_map = {"person": "Person", "location": "Place", "organization": "Organization", "date": "Date",
            "occupation": "Occupation", "technology": "Product", "event": "Event", "work of art": "CreativeWork"}
 
 # Primeira etapa do código é extrair todas as entidades presentes na estrutura, juntamente com seu respectivo tipo.
-# Para isto, extrai-se de cada relação dentro da estrutura JSON a entidade 1 e 2, armazenando elas em um dicionario
+# Para isto, extrai-se de cada relação dentro da estrutura JSON a entidade 1 e 2, armazenando elas em um dicionário
 # 'entities', e estas entidades são então mapeadas a seu tipo. Dentro desta etapa também é realizada uma normalização,
-# caso o código identifique uma entidade já presente em 'entities' mas ela contém um tipo diferente, é adicionado a
-# entidade um parenteses indicando o tipo desta entidade e então ela pode ser armazenada como uma entidade distinta.
+# caso o código identifique uma entidade já presente em 'entities' mas ela contém um tipo diferente, essa entidade é
+# adicionada ao dicionário 'disambiguities' para que seja devidamente tratada ao decorrer do código.
 
 for relation in relations:
     if relation["entity 1"] in entities.keys() and relation["entity 1 type"] != entities[relation["entity 1"]]:
-        disambiguity = relation["entity 1"] + f" ({relation['entity 1 type']})"
-        entities[disambiguity] = relation["entity 1 type"]
+        disambiguities[relation["entity 1"]] = uri_map.get(relation["entity 1 type"], 'Thing')
     else:
         entities[relation["entity 1"]] = relation["entity 1 type"]
     if relation["entity 2"] in entities.keys() and relation["entity 2 type"] != entities[relation["entity 2"]]:
-        disambiguity = relation["entity 2"] + f" ({relation['entity 2 type']})"
-        entities[disambiguity] = relation["entity 2 type"]
+        disambiguities[relation["entity 2"]] = uri_map.get(relation["entity 2 type"], 'Thing')
     else:
         entities[relation["entity 2"]] = relation["entity 2 type"]
 
@@ -55,21 +54,33 @@ for relation in relations:
 rdf_file.write("@prefix ex: <https://example.org/> .\n@prefix sch: <https://schema.org/> .\n\n")
 
 # Nesta etapa são escritas dentro do arquivo "graph.txt" todas as relações identificadas, criando triplas
-# <Sujeito-verbo-predicado> com uma URI básica.
+# <Sujeito-verbo-predicado> com uma URI básica. Caso uma destas triplas tenha uma das entidades ambíguas, o
+# programa é capaz de detectar isso pelo mapeamento entre o nome da entidade e seu tipo não existir no 
+# dicionário 'entities', então para resolver a desambiguidade, é adicionado ao nome da entidade o tipo que ela
+# representa em parenteses, criando uma entidade distinta.
 
 for relation in relations:
-    e1, _, r, e2, _ = relation.values()
+    e1, e1t, r, e2, e2t = relation.values()
+    if entities[e1] != e1t:
+        e1 += f" ({disambiguities[e1]})"
+    if entities[e2] != e2t:
+        e2 += f" ({disambiguities[e2]})"
     rdf_file.write(f"ex:{underlined(e1)} ex:{camel_case(r)} ex:{underlined(e2)} .\n")
 
 rdf_file.write("\n")
 
-# Por último, se utiliza do dicionário 'entity' criado anteriormente para delimitar o tipo de cada entidade detectada
+# Por último, se utiliza do dicionário 'entities' criado anteriormente para delimitar o tipo de cada entidade detectada
 # pelo protótipo, incluindo todos os tipos encontrados ao grafo. O dicionário 'uri-map' mapeia cada tipo previsto na
 # detecção de entidades para o seu esquema válido dentro da URL Schema.org, caso o programa identifique uma entidade que
 # não foi prevista pelo código, sua URI passa a ser Schema.org/Thing, que equivale ao tipo mais genérico de item dentro
-# do sistema.
+# do sistema. O mesmo método de desambiguação é feito aqui, exceto que tudo que é necessário é percorrer também
+# o dicionário 'disambiguities'.
 
 for entity, type in entities.items():
+    rdf_file.write(f"ex:{underlined(entity)} a sch:{uri_map.get(type, 'Thing')} .\n")
+
+for entity, type in disambiguities.items():
+    entity += f" ({disambiguities[entity]})"
     rdf_file.write(f"ex:{underlined(entity)} a sch:{uri_map.get(type, 'Thing')} .\n")
 
 f.close()
